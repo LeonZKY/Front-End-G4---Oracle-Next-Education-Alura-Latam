7a2ec78b8521527c1e53f71ce8949a7f4ec1ba96
Leon Liponezky <112138682+LeonZKY@users.noreply.github.com>
2023-04-11 15:18:21 -0300

    Add files via upload

---
+++ Curso de JS en la Web Manipulación del DOM con JavaScript/components/eliminarIcono.js
@@ -0,0 +1,14 @@
+const eliminarIcono = () => {
+    const i = document.createElement("i");
+    i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+    i.addEventListener("click", deleteTask);
+
+    return i;
+}
+
+const deleteTask = (event) => {
+    const parent = event.target.parentElement;
+    parent.remove();
+};
+
+export default eliminarIcono
